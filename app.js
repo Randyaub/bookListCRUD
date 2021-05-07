f442d2f8bd32bb3f0a588526e381a2a62f5a21af
Randy Aubichon <randal.aub@sasktel.net>
2021-05-06 18:00:32 -0600

    added cors for my front end

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const morgan = require("morgan");
 const helmet = require("helmet");
+const cors = require("cors");
 const booksRouter = require("./api/routes/books");
 require("dotenv").config();
 
 const app = express();
 
 //middlewares
+app.use(cors({ origin: "http://localhost:3000" }));
 app.use(morgan("dev"));
 app.use(helmet());
 app.use(express.json());
